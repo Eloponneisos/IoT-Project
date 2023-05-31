72ac5e4a5a33e32bb4131ba741dc80cd4f617569
Nick Bosschaert <45479517+Eloponneisos@users.noreply.github.com>
2023-05-31 21:20:53 +0200

    Adding of the last of the arduino sketch changes and starting on the readme file and adding a screen of the latest schematic 31-05-2023

---
+++ Arduino/Final/Final.ino
@@ -0,0 +1,407 @@
+//------MCP23016------
+#include <MCP23016.h>
+
+MCP23016 MCP;
+
+#define GPA 0x00  //port A data register address
+#define GPB 0x01  //port B data register address
+
+uint8_t mcpA;  //internal variable of the library - required to be declared as is
+uint8_t mcpB;  //internal variable of the library - required to be declared as is
+
+//------Status lights-----
+#define problem a0
+#define obstacle a1
+#define line a2
+#define onOFF a3
+
+//------sensor------
+#define lineSensorLL b0
+#define lineSensorL b1
+#define lineSensorC b2
+#define lineSensorR b3
+#define lineSensorRR b4
+
+#define collisionSensor b5
+
+int sensors[6] = { lineSensorLL, lineSensorL, lineSensorC, lineSensorR, lineSensorRR, collisionSensor };
+
+String sensornames[8] = { "LL", "L", "C", "R", "RR", "Col", "Line", "Dist" };
+
+bool value[8] = { false, false, false, false, false, false, false, false };
+
+bool middle = false;
+bool left = false;
+bool right = false;
+
+//------Motor Controls------
+#define M1_EN 15
+#define M1_F 2
+#define M1_R 4
+
+#define M2_EN 23
+#define M2_F 18
+#define M2_R 19
+
+#define high 120
+#define low 0
+
+//------Line Controls------
+bool whiteLine = true;
+
+bool haltControl = true;
+int haltLast = 0;
+int elapsedHalt = 0;
+int currentHalt = 0;
+
+#define haltButton 32
+
+//-----Distance Sensor-----
+#define trigPin 33
+#define echoPin 25
+long duration, cm;
+int triggerDistance = 10;
+
+//------Battery Voltage------
+#define BAT 26
+
+float VBAT;
+float R1 = 3259.0;
+float R2 = 983.0;
+
+#define PIN_RED 14
+#define PIN_GREEN 13
+#define PIN_BLUE 12
+
+//------Startup-Tune------
+#define NOTE_CS4 277
+#define NOTE_D4 294
+#define NOTE_E4 330
+#define NOTE_FS4 370
+#define NOTE_GS4 415
+#define NOTE_A4 440
+#define NOTE_B4 494
+#define NOTE_CS5 554
+#define NOTE_D5 587
+#define NOTE_DS5 622
+#define NOTE_E5 659
+#define REST 0
+
+int tempo = 180;
+
+int buzzer = 27;
+
+int melody[] = {
+  NOTE_E5,8,NOTE_D5,8,NOTE_FS4,4,NOTE_GS4,4,
+  NOTE_CS5,8,NOTE_B4,8,NOTE_D4,4,NOTE_E4,4,
+  NOTE_B4,8,NOTE_A4,8,NOTE_CS4,4,NOTE_E4,4,
+  NOTE_A4,2,
+};
+
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
+void setup() {
+  //------Serial Communication------
+  Serial.begin(115200);
+  Serial.println("REBOOT");
+
+  //------Motor pins------
+  pinMode(M1_EN, OUTPUT);
+  pinMode(M1_F, OUTPUT);
+  pinMode(M1_R, OUTPUT);
+
+  pinMode(M2_EN, OUTPUT);
+  pinMode(M2_F, OUTPUT);
+  pinMode(M2_R, OUTPUT);
+
+  //-----Distance Sensor Configuration-----
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  //------Halt Button------
+  pinMode(haltButton, INPUT_PULLUP);
+
+  //------MCP23016 I/O Expander------
+  Wire.begin();
+  Wire.setClock(100000);
+
+  MCP.init(0x20);
+
+  MCP.pinMode(lineSensorLL, INPUT);
+  MCP.pinMode(lineSensorL, INPUT);
+  MCP.pinMode(lineSensorC, INPUT);
+  MCP.pinMode(lineSensorR, INPUT);
+  MCP.pinMode(lineSensorRR, INPUT);
+
+  MCP.pinMode(collisionSensor, INPUT);
+
+  MCP.pinMode(problem, OUTPUT);
+  MCP.pinMode(obstacle, OUTPUT);
+  MCP.pinMode(line, OUTPUT);
+  MCP.pinMode(onOFF, OUTPUT);
+
+  //------VBAT------
+  pinMode(BAT, INPUT);
+
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+
+  analogWrite(PIN_RED, 0);
+  analogWrite(PIN_GREEN, 0);
+  analogWrite(PIN_BLUE, 0);
+
+  //------Startup-Tune-----
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = melody[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(buzzer, melody[thisNote], noteDuration * 0.9);
+    delay(noteDuration);
+    noTone(buzzer);
+
+  }
+  Serial.println("END BOOT");
+}
+
+void loop() {
+
+  batteryCheck();
+
+  if (VBAT >= 8) {
+    analogWrite(PIN_RED, 0);
+    analogWrite(PIN_GREEN, 255);
+    analogWrite(PIN_BLUE, 0);
+  } else if (VBAT < 8 && VBAT >= 6) {
+    analogWrite(PIN_RED, 0);
+    analogWrite(PIN_GREEN, 0);
+    analogWrite(PIN_BLUE, 255);
+  } else if (VBAT < 6) {
+    analogWrite(PIN_RED, 255);
+    analogWrite(PIN_GREEN, 0);
+    analogWrite(PIN_BLUE, 0);
+  }
+
+  distanceCalc();
+  Serial.println("Dist: " + String(cm) + " cm");
+
+  if (cm < triggerDistance) {
+    value[7] = true;
+  } else {
+    value[7] = false;
+  }
+
+  MCP.digitalWrite(onOFF, HIGH);
+
+  if (whiteLine) {
+    //------Sensor data acquisition for a white line----
+    for (int i = 0; i < 6; i++) {
+      value[i] = MCP.digitalRead(sensors[i]);
+    }
+  } else {
+    //------Sensor data acquisition for a black line----
+    for (int i = 0; i < 6; i++) {
+      value[i] = !MCP.digitalRead(sensors[i]);
+    }
+    value[5] = !value[5];
+  }
+
+
+
+  for (int i = 0; i < 8; i++) {
+    Serial.print(String(sensornames[i]));
+    Serial.print(": ");
+    Serial.print(value[i]);
+    Serial.print(" | ");
+  }
+  Serial.println();
+
+  if (value[5] == false && value[6] == false && value[7] == false) {
+    MCP.digitalWrite(problem, HIGH);
+  } else {
+    MCP.digitalWrite(problem, LOW);
+  }
+
+  if (value[5] == true || value[7] == true) {
+    MCP.digitalWrite(obstacle, HIGH);
+  } else {
+    MCP.digitalWrite(obstacle, LOW);
+  }
+
+  if (value[6] == true) {
+    MCP.digitalWrite(line, HIGH);
+  } else {
+    MCP.digitalWrite(line, LOW);
+  }
+
+  //------Stand still at obstacle------
+  if (value[5] == HIGH || value[7] == HIGH) {
+    digitalWrite(M1_EN, LOW);
+    analogWrite(M1_F, low);
+    analogWrite(M1_R, low);
+    digitalWrite(M2_EN, LOW);
+    analogWrite(M2_F, low);
+    analogWrite(M2_R, low);
+    middle = false;
+    left = false;
+    right = false;
+    value[6] = false;
+  }
+
+  //------White line controll------
+  else if (((value[1] == HIGH) && (value[2] == HIGH) && (value[3] == HIGH) && (value[5] == LOW) && (value[7] == LOW)) || haltControl == false) {
+    digitalWrite(M1_EN, LOW);
+    analogWrite(M1_F, low);
+    analogWrite(M1_R, low);
+    digitalWrite(M2_EN, LOW);
+    analogWrite(M2_F, low);
+    analogWrite(M2_R, low);
+    Serial.println("Halt point reached");
+    if (haltControl) {
+      haltControl = false;
+      haltLast = millis();
+    }
+    currentHalt = millis();
+    if (((currentHalt - haltLast) >= 5000) || digitalRead(haltButton) == LOW) {
+      currentHalt = haltLast + 5500;
+      haltControl = true;
+      digitalWrite(M1_EN, HIGH);
+      analogWrite(M1_F, high);
+      analogWrite(M1_R, low);
+      digitalWrite(M2_EN, HIGH);
+      analogWrite(M2_F, high);
+      analogWrite(M2_R, low);
+      delay(200);
+    }
+    middle = false;
+    left = false;
+    right = false;
+    value[6] = false;
+  }
+
+  //------Line following: center white------
+  else if ((value[1] == LOW) && (value[2] == HIGH) && (value[3] == LOW) && (value[5] == LOW) && (value[7] == LOW)) {
+    digitalWrite(M1_EN, HIGH);
+    analogWrite(M1_F, high);
+    analogWrite(M1_R, low);
+    digitalWrite(M2_EN, HIGH);
+    analogWrite(M2_F, high);
+    analogWrite(M2_R, low);
+    middle = true;
+    left = false;
+    right = false;
+    value[6] = false;
+  }
+
+  //------Line following: Left white------
+  else if ((value[1] == HIGH) && (value[2] == LOW) && (value[3] == LOW) && (value[5] == LOW) && (value[7] == LOW)) {
+    digitalWrite(M1_EN, HIGH);
+    analogWrite(M1_F, high);
+    analogWrite(M1_R, low);
+    digitalWrite(M2_EN, HIGH);
+    analogWrite(M2_F, low);
+    analogWrite(M2_R, high);
+    middle = false;
+    left = true;
+    right = false;
+    value[6] = false;
+  }
+
+  //------Line following: Right white------
+  else if ((value[1] == LOW) && (value[2] == LOW) && (value[3] == HIGH) && (value[5] == LOW) && (value[7] == LOW)) {
+    digitalWrite(M1_EN, HIGH);
+    analogWrite(M1_F, low);
+    analogWrite(M1_R, high);
+    digitalWrite(M2_EN, HIGH);
+    analogWrite(M2_F, high);
+    analogWrite(M2_R, low);
+    middle = false;
+    left = false;
+    right = true;
+    value[6] = false;
+  }
+
+  //------Line lost check------
+  else if ((value[1] == LOW) && (value[2] == LOW) && (value[3] == LOW) && (value[5] == LOW) && (value[7] == LOW)) {
+    if (middle == true) {
+      digitalWrite(M1_EN, HIGH);
+      analogWrite(M1_F, high);
+      analogWrite(M1_R, low);
+      digitalWrite(M2_EN, HIGH);
+      analogWrite(M2_F, high);
+      analogWrite(M2_R, low);
+    } else if (left == true) {
+      digitalWrite(M1_EN, HIGH);
+      analogWrite(M1_F, low);
+      analogWrite(M1_R, high);
+      digitalWrite(M2_EN, HIGH);
+      analogWrite(M2_F, high);
+      analogWrite(M2_R, low);
+    } else if (right == true) {
+      digitalWrite(M1_EN, HIGH);
+      analogWrite(M1_F, high);
+      analogWrite(M1_R, low);
+      digitalWrite(M2_EN, HIGH);
+      analogWrite(M2_F, low);
+      analogWrite(M2_R, high);
+    } else {
+      digitalWrite(M1_EN, LOW);
+      analogWrite(M1_F, low);
+      analogWrite(M1_R, low);
+      digitalWrite(M2_EN, LOW);
+      analogWrite(M2_F, low);
+      analogWrite(M2_R, low);
+      middle = false;
+      left = false;
+      right = false;
+      value[6] = true;
+      Serial.println("Lost the line");
+    }
+  }
+
+  //------Unforeseen state------
+  else {
+    Serial.println("ERROR");
+    value[6] = false;
+  }
+
+  //------Printing the states------
+  if (middle == true) {
+    Serial.println("Middle");
+  } else if (left == true) {
+    Serial.println("left");
+  } else if (right == true) {
+    Serial.println("right");
+  } else {
+    Serial.println("No line");
+  }
+}
+
+//------Checking the battery voltage------
+void batteryCheck() {
+  VBAT = (((R1 + R2) / R2) * (3.30f / 4095.0f) * analogRead(BAT)) + 0.5f;
+  Serial.print("Battery Voltage = ");
+  Serial.print(VBAT, 2);
+  Serial.println(" V");
+}
+
+//------Calculating the distance to the nearest object infront of the sensor------
+void distanceCalc() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = (duration / 2) * 0.0343;
+}
--- README.md
+++ README.md
@@ -1,8 +1,39 @@
 # Autonomous line-following car for Mercedes Benz
 
+- [Autonomous line-following car for Mercedes Benz](#autonomous-line-following-car-for-mercedes-benz)
+  - [Intro](#intro)
+  - [Components](#components)
+  - [The Schematic](#the-schematic)
+
+
+## Intro
+In this project I will make an autonomous line-tracking car. The car is a prototype for the Mercedes Benz museum. Once done, the car will be able to perform the following actions:
+1. Follow a white line
+2. Detect obstacles in it's way using a HC-SR04 sensor
+3. Detect if it has collided with anything
+4. Show the battery voltage through a RGB-led indicator
+5. Show the status of the vehicle through 4 different leds
+6. It'll have a startup-tune
+7. Web interface (grafana)
+8. It'll also be able to stop at a traversal line at which point it either starts driving after 5sec or after the pressing of a button
+
 ## Components
 1. ESP-32
-2. 1 5xTCRT5000 IR sensor
-3. 2 5V motor
-4. Ultrasoon sensor
-5. 4 leds (green, orange, red, blue) 
+2. 1 5xTCRT5000 IR sensor with Limit Switch
+3. 2 5V Motor
+4. 1 L293d Motor Driver
+5. 1 LM 1117 T5,0 Voltage Regulator
+6. 2 9V Batteries
+7. 1 Switch
+8. 1 DC-female jack
+9. 1 RGB-led
+10. 1 MCP23016 I/O Expander
+11. 1 HC-SR04 Ultrasone Sensor
+12. 4 leds (green, orange, red, blue) 
+13. 1 Button
+14. 1 Buzzer
+15. 1 Castor Wheel
+16. Wires
+
+## The Schematic
+![Schematic](https://imgur.com/wjlqvnd.png)
